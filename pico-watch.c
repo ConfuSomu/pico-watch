d113cf3c7c1a0f68a07be26b460a68547fdf34c8
ConfuSomu <emilemeekel+github@gmail.com>
2021-02-07 00:00:40 -0500

    Show date

--- pico-watch.c
+++ pico-watch.c
@@ -20,7 +20,7 @@ void init_display() {
     oledInit(&oled, OLED_128x64, 0x3d, 0, 0, 1, SDA_PIN, SCL_PIN, RESET_PIN, 1000000L);
     oledFill(&oled, 0,1);
     oledSetContrast(&oled, 127);
-    oledSetTextWrap(&oled, true);
+    //oledSetTextWrap(&oled, true);
 }
 
 void init_rtc() {
@@ -46,15 +46,38 @@ void time_as_str(char *buf, uint buf_size, const datetime_t *t) {
              t->min,
              t->sec);
 };
+void date_as_str(char *buf, uint buf_size, const datetime_t *t) {
+    static const char *DATETIME_DOWS[7] = {
+        "Sun",
+        "Mon",
+        "Tue",
+        "Wed",
+        "Thu",
+        "Fri",
+        "Sat",
+    };
+    snprintf(buf,
+             buf_size,
+             "%02d-%02d-%02d %s",
+             t->year,
+             t->month,
+             t->day,
+             DATETIME_DOWS[t->dotw - 1]);
+};
 
 void show_datetime() {
     char datetime_buf[256];
     char *datetime_str = &datetime_buf[0];
     datetime_t t;
-
     rtc_get_datetime(&t);
+
+    // time
     time_as_str(datetime_str, sizeof(datetime_buf), &t);
     oledWriteString(&oled, 0,10,3, datetime_str, FONT_12x16, 0, 1);
+
+    // date
+    date_as_str(datetime_str, sizeof(datetime_buf), &t);
+    oledWriteString(&oled, 0,0,7, datetime_str, FONT_8x8, 0, 1);
 }
 
 int main()
