b02cd462dce7095d55def3a014fe51eddc238088
ConfuSomu <emilemeekel+github@gmail.com>
2021-05-27 19:43:32 -0400

    Use pragma once in main.hpp
    #pragma once will be used in every header file from now on. Changes will
    be made for the other header files.

--- apps/settings/main.hpp
+++ apps/settings/main.hpp
@@ -1,5 +1,4 @@
-#ifndef __SETTINGS_H__
-#define __SETTINGS_H__
+#pragma once
 #include "../../api.hpp"
 
 namespace app_settings {
@@ -9,5 +8,3 @@ namespace app_settings {
     int bgrefresh(Api *app_api, bool in_foreground);
     int destroy(Api *app_api);
 }
-
-#endif
