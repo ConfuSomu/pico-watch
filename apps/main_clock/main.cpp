1abdb3ef5968a6267f6e4643fea7c7962a5a4af4
ConfuSomu <emilemeekel+github@gmail.com>
2021-09-03 22:59:53 -0400

    Clean up spacing and remove uneeded comments

--- apps/main_clock/main.cpp
+++ apps/main_clock/main.cpp
@@ -18,15 +18,7 @@ void app_main_clock::time_as_str(char *buf, uint buf_size, const datetime_t *t) 
 };
 
 void app_main_clock::date_as_str(char *buf, uint buf_size, const datetime_t *t) {
-    static const char *DATETIME_DOWS[7] = {
-        "Sun",
-        "Mon",
-        "Tue",
-        "Wed",
-        "Thu",
-        "Fri",
-        "Sat",
-    };
+    static const char *DATETIME_DOWS[7] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
     snprintf(buf,
             buf_size,
             "%02d-%02d-%02d %s",
@@ -51,7 +43,6 @@ void app_main_clock::show_datetime(Api *app_api) {
     app_api->gui_footer_text((std::string)datetime_str);
 }
 
-// Rendering of the app
 BaseApp::AppReturnValues app_main_clock::render(Api *app_api) {
     app_api->gui_header_text("Test clock", 17);
     show_datetime(app_api);
@@ -59,21 +50,17 @@ BaseApp::AppReturnValues app_main_clock::render(Api *app_api) {
     return AppReturnValues::OK;
 }
 
-// Interpretation of button inputs
 BaseApp::AppReturnValues app_main_clock::btn_pressed(Api *app_api, uint gpio, unsigned long delta) {
     return AppReturnValues::OK;
 }
 
-// Initlisation of the app.
 app_main_clock::app_main_clock(Api *app_api) {
     app_api->performance_set(Api::perf_modes::LOW_POWER);
 }
 
-// Processor intensive operations and functions related to drawing to the screen should only be done when the app is in_foreground(=1). This function is only called when the app is init.
 BaseApp::AppReturnValues app_main_clock::bgrefresh(Api *app_api, bool in_foreground) {
     return AppReturnValues::OK;
 }
 
-// Destruction of app, deinitlisation should be done here. This is only called if the app's APPS_DESTROY_ON_EXIT is set to 1. When it is not a "service" app.
 app_main_clock::~app_main_clock() {
 }
